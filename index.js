44fdade4e7e2890b2a34ddffa755531293e19b53
James Oliveira <135138774+JamesOliveira023@users.noreply.github.com>
2023-08-24 11:32:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+document.getElementById('card').style.setProperty(' display' , 'none' , 'important')
+
+function Procurar(){
+    document.getElementById('card').style.setProperty(' display' , 'block' , 'important')
+
+    let pais = document.getElementById('pais').value
+    let finalURL = `https://restcountries.com/v3.1/name/${pais}?fullText=true`
+console.log(finalURL)
+fetch(finalURL)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data[0])
+    console.log(data[0].population)
+    console.log(data[0].capital[0])
+    console.log(data[0].continents[0])
+    
+    let bandeira = document.getElementById('bandeira')
+    let nome = document.getElementById('nome')
+    let Capital = document.getElementById('Capital')
+    let Pop = document.getElementById('Pop')
+    let Cont = document.getElementById('Cont')
+
+    nome.innerHTML = pais
+    Capital.innerHTML = data[0].capital[0]
+    Cont.innerHTML = data[0].continents[0]
+    Pop.innerHTML = data[0].population
+    bandeira.src = data[0].flags.svg
+})
+}
